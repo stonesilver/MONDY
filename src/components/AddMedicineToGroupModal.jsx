e00cb00d9894a29a6efd41a605e47d3b32c888d2
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-14 22:17:28 +0100

    completed modal UI

--- src/components/AddMedicineToGroupModal.jsx
+++ src/components/AddMedicineToGroupModal.jsx
@@ -2,10 +2,12 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { ReactComponent as CloseIcon } from '../assets/svg/close-circle.svg';
+import { ReactComponent as AddIcon } from '../assets/svg/add.svg';
+import SearchBar from './base/SearchBar';
+import Button from './base/Button';
 
 const style = {
   position: 'absolute',
@@ -17,13 +19,13 @@ const style = {
   bgcolor: 'white.main',
   boxShadow: 24,
   borderRadius: '4px',
-  p: 1,
+  py: 1,
+  px: 4,
 };
 
 const AddMedicineToGroupModal = ({ open, handleOpen, handleClose }) => {
   return (
     <div>
-      <Button onClick={handleOpen}>Open modal</Button>
       <Modal
         aria-labelledby='transition-modal-title'
         aria-describedby='transition-modal-description'
@@ -37,15 +39,29 @@ const AddMedicineToGroupModal = ({ open, handleOpen, handleClose }) => {
       >
         <Fade in={open}>
           <Box sx={style}>
-            <IconButton sx={{ float: 'right' }}>
-              <CloseIcon />
-            </IconButton>
-            <Typography variant='h5' component='h2'>
+            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <IconButton onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+            </Box>
+
+            <Typography variant='h4' component='h2'>
               Add Medicine
             </Typography>
-            <Typography id='transition-modal-description' sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            <Typography mt={2} mb='2px'>
+              Medicine
             </Typography>
+            <SearchBar placeholder='Enter Medicine Name or Medicine ID' />
+            <Box mt={4}>
+              <Button
+                icon={<AddIcon />}
+                text='Add Medicine to Group'
+                bgColor='red.main'
+                textColor='white.main'
+                variant='contained'
+                handleClick={() => alert('Medicine added to group!!!')}
+              />
+            </Box>
           </Box>
         </Fade>
       </Modal>
