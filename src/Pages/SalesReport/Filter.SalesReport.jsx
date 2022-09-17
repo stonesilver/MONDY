a18127e7db867d4dc23b40dedad25ace6bbb3f04
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-17 06:23:17 +0100

    coe update filter component

--- src/Pages/SalesReport/Filter.SalesReport.jsx
+++ src/Pages/SalesReport/Filter.SalesReport.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import CustomSelect from '../../components/base/Select';
 
 const SalesReportFilter = () => {
+  const [state, setState] = useState({
+    medicineGroup: '- Select Group -',
+    userName: '- Select User Name -',
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setState((prevS) => ({ ...prevS, [name]: value }));
+  };
+
+  const { medicineGroup, userName } = state;
+
   return (
     <Box
       sx={{ display: 'flex', justifyContent: 'space-between', gap: 3, mt: 2 }}
@@ -13,10 +25,10 @@ const SalesReportFilter = () => {
         <Typography mb='3px'>Medicine Group</Typography>
         <CustomSelect
           options={[]}
-          value='- Select Group -'
+          value={medicineGroup}
           placeholder='- Select Group -'
           name='medicineGroup'
-          handleChange={() => {}}
+          handleChange={handleChange}
           bgcolor='#b7c1ca6e'
         />
       </Box>
@@ -24,10 +36,10 @@ const SalesReportFilter = () => {
         <Typography mb='3px'>User Name</Typography>
         <CustomSelect
           options={[]}
-          value='- Select User Name -'
+          value={userName}
           placeholder='- Select User Name -'
           name='userName'
-          handleChange={() => {}}
+          handleChange={handleChange}
           bgcolor='#b7c1ca6e'
         />
       </Box>
