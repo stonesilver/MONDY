852760093241c8f9e7427d239c5c4cc82c2e7793
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-17 19:35:45 +0100

    fixed import issue

--- src/Pages/MedicineList/MedicineList.jsx
+++ src/Pages/MedicineList/MedicineList.jsx
@@ -1,7 +1,7 @@
 import Box from '@mui/material/Box';
 import MedicineListTable from './MedicineListTable';
 import SearchFilter from './SearchFilter';
-import TitleBar from './TitleBar.medicineList';
+import TitleBar from './TitleBar.MedicineList';
 
 const MedicineList = () => {
   return (
