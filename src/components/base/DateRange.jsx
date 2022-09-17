23cb9f43a706a66c29c35d7bc260420a56cb16b2
ezenwa chigozie <stonesilver12@gmail.com>
2022-09-17 18:11:41 +0100

    updated date range component

--- src/components/base/DateRange.jsx
+++ src/components/base/DateRange.jsx
@@ -2,21 +2,50 @@ import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import moment from 'moment';
 import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
 
 const DateRange = () => {
-  const [value, setValue] = useState(moment().format());
+  const [value, setValue] = useState({
+    from: moment().format(),
+    to: moment().format(),
+  });
+
+  const handleChange = (newValue, key) => {
+    setValue((prevS) => ({ ...prevS, [key]: newValue }));
+  };
+
+  const { from, to } = value;
 
   return (
     <LocalizationProvider dateAdapter={AdapterMoment}>
-      <Stack spacing={3}>
+      <Stack direction='row' alignItems='center'>
         <MobileDatePicker
-          value={moment(value).format('LL')}
+          value={moment(from).format('LL')}
           onChange={(newValue) => {
-            setValue(newValue);
+            handleChange(newValue, 'from');
           }}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              sx={{
+                '& .MuiInputBase-input': {
+                  height: 8,
+                  bgcolor: '#b7c1ca6e',
+                },
+              }}
+            />
+          )}
+        />
+        <Box mx={1}>-</Box>
+        <MobileDatePicker
+          value={moment(to).format('LL')}
+          onChange={(newValue) => {
+            handleChange(newValue, 'to');
+          }}
+          minDate={moment(from).format('LL')}
           renderInput={(params) => (
             <TextField
               {...params}
